d594422547011a5407da3072bb42e80e0856f654
Mukarram685 <162946925+Mukarram685@users.noreply.github.com>
2025-10-17 15:37:33 +0500

    splash screen setup

--- android/app/src/main/java/com/bookngo/MainActivity.kt
+++ android/app/src/main/java/com/bookngo/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.bookngo
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript.
    */
   override fun getMainComponentName(): String = "BookNGo"
 
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Show splash screen before React content is loaded.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this) // Show the native splash
+    super.onCreate(savedInstanceState)
+  }
+
+  /**
+   * Returns the ReactActivityDelegate instance.
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
