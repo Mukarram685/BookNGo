be827e8c520bb4c1d1725dad4ef7ef9f6127a130
Mukarram685 <muka654r@gmail.com>
2026-01-30 19:12:36 +0500

    eslint rule will be changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,60 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+
+  plugins: [
+    'react',
+    'react-native',
+    '@typescript-eslint',
+  ],
+
+  rules: {
+    /* 🔴 TypeScript-safe unused checks */
+    'no-unused-vars': 'off', // turn off JS version
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+
+    'react-native/no-inline-styles': 'off',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
