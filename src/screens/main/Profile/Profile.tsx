be649e3de44a58e219b18b1c617a6a89c284c03f
Mukarram685 <muka654r@gmail.com>
2026-01-30 19:40:33 +0500

    remaing profile screen

---
+++ src/screens/main/Profile/Profile.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import { useDispatch } from 'react-redux';
+import ScreenWrapper from '../../../component/common/ScreenWrapper';
+import AppText from '../../../component/common/AppText';
+import Colors from '../../../utils/Colors.util';
+import Header from '../../../component/Header';
+import { logout } from '../../../store/slice/auth.slice';
+
+const Profile = () => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+    };
+
+    return (
+        <ScreenWrapper backgroundColor={Colors.DARK_BG} header={<Header title="Profile" showBack={false} />}>
+            <StatusBar barStyle="light-content" backgroundColor={Colors.DARK_BG} />
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.logoutButton} onPress={handleLogout} activeOpacity={0.8}>
+                    <AppText size={16} weight="700" color={Colors.WHITE}>
+                        Logout
+                    </AppText>
+                </TouchableOpacity>
+            </View>
+        </ScreenWrapper>
+    );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    logoutButton: {
+        backgroundColor: Colors.RED,
+        paddingVertical: 14,
+        borderRadius: 12,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+});
